475acd7eb594cc638dec5c267c1c2e02c4b2385a
Wictor Nogueira <wictorgoesnogueira@gmail.com>
2021-03-26 23:53:11 -0300

    restrict devTools script scope

--- src/scripts/devTools.js
+++ src/scripts/devTools.js
@@ -1,9 +1,11 @@
-const remote = require('electron').remote;
-const win = remote.getCurrentWindow();
-
-document.addEventListener ('keydown', (e) => {
-  if (e.keyCode === 123) {
-    e.preventDefault();
-    win.openDevTools();
-  }
-});
+(function(){
+  const remote = require('electron').remote;
+  const win = remote.getCurrentWindow();
+  
+  document.addEventListener ('keydown', (e) => {
+    if (e.keyCode === 123) {
+      e.preventDefault();
+      win.openDevTools();
+    }
+  });
+})();
